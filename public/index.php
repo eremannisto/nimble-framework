c9c03e28aa8ffe0c7a5e985fffc033c0b34cd49c
Ere Männistö <connect@eremannisto.com>
2023-08-21 16:54:26 +0300

    Version 1.0.0

--- public/index.php
+++ public/index.php
@@ -14,7 +14,6 @@ Head::global([
         '<link rel="preconnect" href="https://fonts.googleapis.com">',
         '<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>',
         '<link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">',
-        '<script src="https://kit.fontawesome.com/10c60e7810.js" crossorigin="anonymous"></script>',
     ],
 
     "scripts" => [
